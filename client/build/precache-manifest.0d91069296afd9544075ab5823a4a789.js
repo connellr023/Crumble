bf3192b390efc9f8f04ce9c76f66e5afad0abd4a
Connell Reffo <connellreffo@DESKTOP-MU8DQG3.hitronhub.home>
2020-08-18 18:07:44 -0600

    Updated Git Ignore

---
+++ client/build/precache-manifest.0d91069296afd9544075ab5823a4a789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccbf6471fe33048239a3f6a8c638b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b992b71c18c9b5520d52",
+    "url": "/static/css/main.93aa31b1.chunk.css"
+  },
+  {
+    "revision": "b6245a05b4d7703add31",
+    "url": "/static/js/2.abb811e8.chunk.js"
+  },
+  {
+    "revision": "7b7e02d4bced4eea42867b10a0a4d7e3",
+    "url": "/static/js/2.abb811e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b992b71c18c9b5520d52",
+    "url": "/static/js/main.35944873.chunk.js"
+  },
+  {
+    "revision": "3d4109d48f82c85e5fdb",
+    "url": "/static/js/runtime-main.8339301f.js"
+  },
+  {
+    "revision": "719e2b9fc2e3d43f96aa9ad4225fcfc0",
+    "url": "/static/media/crumble.719e2b9f.ttf"
+  }
+]);
